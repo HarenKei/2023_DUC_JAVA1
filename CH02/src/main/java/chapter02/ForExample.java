384ef33f58a663abc41cd2dd701e633888b044a7
HarenKei <seungmin1266@gmail.com>
2023-03-10 15:19:43 +0900

    Add : ForExample

---
+++ CH02/src/main/java/chapter02/ForExample.java
@@ -0,0 +1,14 @@
+package chapter02;
+
+public class ForExample {
+    public static void main(String[] args) {
+        for(int m = 2; m <= 9; m++){
+            System.out.println("*** " + m + "단 ***");
+            for(int n = 1; n <= 9; n++){
+                System.out.println(m + " x " + n + " = " + n * n);
+            }
+            System.out.println();
+        }
+
+    }
+}
