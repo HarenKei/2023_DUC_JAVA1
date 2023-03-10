85dbeabe0fe29da792469840e205a003e43939d2
HarenKei <seungmin1266@gmail.com>
2023-03-10 14:24:17 +0900

    Add : CH02 - ScannerExample

--- CH02/src/main/java/chapter02/VariableExample.java
+++ CH02/src/main/java/chapter02/VariableExample.java
@@ -2,6 +2,21 @@ package chapter02;
 
 public class VariableExample {
     public static void main(String[] args) {
+        int hour = 3;
+        int minutes = 5;
 
+        System.out.println(hour + "시간 " + minutes + "분");
+
+        int totalMinutes = (hour * 60) + minutes;
+        System.out.println("total : " + totalMinutes);
+
+        int v1, v2 = 0;
+
+        if(true) {
+            v1 = 1;
+            v2 = 2;
+        }
+
+        System.out.println(v1 + v2);
     }
 }
